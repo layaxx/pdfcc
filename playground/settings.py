8e48e0f87f1ca994ded5be1cfbb013411245b0ce
Yannick Lang <layaxx@gmx.de>
2020-11-27 22:03:16 +0100

    update static files

--- playground/settings.py
+++ playground/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -116,3 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(os.path.join(BASE_DIR, 'pdfcc'), 'static')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
