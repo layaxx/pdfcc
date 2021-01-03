92562ad5a4c378ca9ff08bb5bcc127f9a5ef0c3d
Yannick Lang <layaxx@gmx.de>
2021-01-03 21:39:54 +0100

    misc adjustments

--- frontend/src/Components/Download/index.tsx
+++ frontend/src/Components/Download/index.tsx
@@ -1,16 +1,35 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+import beforeUnloadListener from '../listeners';
 
-function Download(props: { b64: () => string, oldFileName: () => string | undefined }) {
+function Download(props: {
+    b64: () => string,
+    oldFileName: () => string | undefined
+}) {
+
     const oldFileName = props.oldFileName();
     const newFileName = oldFileName?.split(".pdf")[0] + "-printable";
+
     return (
         <div>
             <h4 className="mb-3">PDF Document</h4>
             <a
                 href={"data:application/pdf;base64," + props.b64()}
-                className="btn btn-success btn-block" download={newFileName}>
+                className="btn btn-success btn-block"
+                download={newFileName}
+                onClick={() => {
+                    // stop asking for confirmation before leaving page
+                    window.removeEventListener('beforeunload', beforeUnloadListener);
+                }
+                }>
                 Download your new PDF!
                 </a>
+            <p className="next">Do you want to{" "}
+                <a
+                    href="/"
+                >
+                    process another Document?</a>
+            </p>
         </div >
     );
 }
--- frontend/src/Components/FileUpload/index.tsx
+++ frontend/src/Components/FileUpload/index.tsx
@@ -6,10 +6,14 @@ import Spinner from 'react-bootstrap/Spinner';
 import './style.css';
 
 
-function FileUpload(prop: { showColors: (colorArray: Array<Array<String>>, file: File) => void }) {
+function FileUpload(prop: {
+    showColors: (colorArray: Array<Array<String>>,
+        file: File) => void
+}) {
     const [isValid, setIsValid] = useState(false);
     const [file, setFile] = useState({});
     const [alertState, setAlertState] = useState({ message: "", type: "" });
+    const [waitingForResponse, setWaitingForResponse] = useState(false);
 
     function fileIsValid(event: ChangeEvent): Boolean {
         const fileSize: number | undefined = (event?.target as HTMLInputElement)?.files?.item(0)?.size;
@@ -26,22 +30,80 @@ function FileUpload(prop: { showColors: (colorArray: Array<Array<String>>, file:
         return false;
     }
 
+    function handleClick(event: React.MouseEvent): void {
+        event.preventDefault();
+        if (isValid) {
+            var data: FormData = new FormData();
+            const pdf = file as Blob;
+            data.append('file', pdf);
+            setAlertState({
+                message: "Waiting for Server response",
+                type: "info"
+            });
+            setWaitingForResponse(true);
+            fetch("/ajax/react", {
+                method: 'POST',
+                body: data
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw Error(response.statusText);
+                    }
+                    return response;
+                })
+                .then(response => response.json())
+                .then(response => {
+                    if (response.error) {
+                        setAlertState({
+                            message: "Server responded with error message: " + response.message,
+                            type: "danger"
+                        });
+                        setWaitingForResponse(false);
+                    } else {
+                        prop.showColors(response.analysis_result, file as File);
+                    }
+
+                })
+                .catch(function (error) {
+                    setAlertState({
+                        message: "" + error,
+                        type: "danger"
+                    });
+                    setWaitingForResponse(false);
+                });
+        } else {
+            setAlertState({
+                message: "Please select a Document first",
+                type: "warning"
+            });
+        }
+    }
+
     return (
         <div>
             <h4 className="mb-3">PDF Document</h4>
-            <form encType="multipart/form-data" id="id_ajax_upload_form" method="POST" noValidate>
+            <form
+                encType="multipart/form-data"
+                id="id_ajax_upload_form"
+                method="POST"
+                noValidate>
                 <label htmlFor="input_pdf">Choose a PDF Document: </label>
                 <br />
-                <input id="input_pdf" type="file" accept="application/pdf" onChange={(e) => {
-                    if (fileIsValid(e) && e.target.files != null) {
-                        setFile(e.target.files[0])
-                        setIsValid(true);
-                        setAlertState({ message: "", type: "" });
-                    } else {
-                        setIsValid(false);
-                        (document.getElementById("id_ajax_upload_form") as HTMLFormElement)?.reset();
-                    }
-                }} required></input>
+                <input
+                    id="input_pdf"
+                    type="file"
+                    accept="application/pdf"
+                    disabled={waitingForResponse}
+                    onChange={(event) => {
+                        if (fileIsValid(event) && event.target.files != null) {
+                            setFile(event.target.files[0])
+                            setIsValid(true);
+                            setAlertState({ message: "", type: "" });
+                        } else {
+                            setIsValid(false);
+                            (document.getElementById("id_ajax_upload_form") as HTMLFormElement)?.reset();
+                        }
+                    }} required></input>
                 <div className="status-wrapper">
                     <Alert
                         variant={alertState.type}
@@ -50,44 +112,16 @@ function FileUpload(prop: { showColors: (colorArray: Array<Array<String>>, file:
                         <Spinner
                             animation="border"
                             role="status"
-                            hidden={alertState.message !== "Waiting for Server response"}></Spinner>
+                            hidden={!waitingForResponse}></Spinner>
                         {alertState.message}</Alert>
                 </div>
-                <Button id="submit-pdf" type="submit" block onClick={(event: React.MouseEvent) => {
-                    event.preventDefault();
-                    if (isValid) {
-                        var data: FormData = new FormData();
-                        const pdf = file as Blob;
-                        data.append('file', pdf);
-                        setAlertState({ message: "Waiting for Server response", type: "info" });
-                        fetch("/ajax/react", {
-                            method: 'POST',
-                            body: data
-                        })
-                            .then(response => {
-                                if (!response.ok) {
-                                    throw Error(response.statusText);
-                                }
-                                return response;
-                            })
-                            .then(response => response.json())
-                            .then(response => {
-                                if (response.error) {
-                                    setAlertState({ message: "Server responded with error message: " + response.message, type: "danger" });
-                                } else {
-                                    prop.showColors(response.analysis_result, file as File);
-                                }
-
-                            })
-                            .catch(function (error) {
-                                setAlertState({ message: "" + error, type: "danger" });
-                            });
-                    } else {
-                        setAlertState({ message: "Please select a Document first", type: "warning" });
-                    }
-                }}
-                    disabled={alertState.message === "Waiting for Server response"}>
-                    {alertState.message !== "Waiting for Server response" ? "Submit Document" : "Waiting..."}
+                <Button
+                    id="submit-pdf"
+                    type="submit"
+                    onClick={handleClick}
+                    disabled={waitingForResponse}
+                    block>
+                    {!waitingForResponse ? "Submit Document" : "Waiting..."}
                 </Button>
             </form>
         </div >
