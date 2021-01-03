5259f25cf1bdbe87960bf0faa684b85ebe460073
Yannick Lang <layaxx@gmx.de>
2021-01-03 16:27:41 +0100

    major rework and refactor of backend logic

---
+++ backend/pdfcc/utility_process.py
@@ -0,0 +1,82 @@
+import pikepdf
+import io
+from base64 import b64encode
+from .color import color
+
+
+def isValidColor(hexcode):
+    '''
+    takes string as input and returns Boolean
+    returns True iff the provided input is a valid hex color code
+    This function does not care wether input has # prefix
+    '''
+    hexcode = hexcode.lower()
+    valid_characters = ['0', '1', '2', '3', '4', '5', '6',
+                        '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+    if len(hexcode) == 7:
+        if hexcode[0] == '#':
+            hexcode = hexcode[1:7]
+    if len(hexcode) == 6:
+        for char in hexcode:
+            if not char in valid_characters:
+                return False
+        return True
+    return False
+
+
+def process_request(post_data, old_pdf):
+    '''
+    Takes the POST part of the request and a PDF as input
+    POST part is expected to contain at least one valid entry, otherwise Expection is raised
+    valid entries consist of a valid colorcodes for both identifier and value
+    Returns a Base64 encoded PDF with colors substituted according to entries in POST data
+    '''
+    # Part 1: extract color data from POST data
+    dict_of_valid_colors = {}
+    for key in post_data:
+        print(key, isValidColor(key))
+        print(post_data[key], isValidColor(post_data[key]))
+        if isValidColor(key) and isValidColor(post_data[key]):
+            dict_of_valid_colors[key.replace(
+                '#', '')] = color(post_data[key])
+    if len(dict_of_valid_colors) == 0:
+        # raise Exception if no valid entries are found
+        raise BaseException("No valid colors were provided")
+
+    # Part 2: generate a new PDF
+    new_pdf = replace_colors(dict_of_valid_colors, old_pdf)
+
+    # Part 3: B64 encode the new PDF
+    b64 = str(b64encode(new_pdf))[2:-1]
+    return b64
+
+
+def replace_colors(dict_of_colors, old_pdf):
+    '''
+    Takes a dict of colors to be replaced, with hex color code of old color as identifier and
+    color object of new color as value as well as a PDF as input
+    Returns the new PDF as bytes
+    '''
+    list_of_operators = [pikepdf.Operator('sc'), pikepdf.Operator(
+        'SC'), pikepdf.Operator('rg'), pikepdf.Operator('RG')]
+
+    with pikepdf.Pdf.open(old_pdf) as pdf:
+        # iterate over every page in the PDF
+        for pageindex in range(len(pdf.pages)):
+            page = pdf.pages[pageindex]
+            content_stream = pikepdf.parse_content_stream(page)
+            for entry in content_stream:
+                if entry[1] in list_of_operators:
+                    color_old_hexcode = color(entry[0]).asHex()
+                    color_new = dict_of_colors.get(
+                        color_old_hexcode, color(color_old_hexcode))
+                    if not color_new.asHex() == color_old_hexcode:
+                        color_new_rgb_list = color_new.asRGB1()
+                        entry[0][0] = color_new_rgb_list[0]
+                        entry[0][1] = color_new_rgb_list[1]
+                        entry[0][2] = color_new_rgb_list[2]
+            content_stream_new = pikepdf.unparse_content_stream(content_stream)
+            page.Contents = pdf.make_stream(content_stream_new)
+        stream_output = io.BytesIO()
+        pdf.save(stream_output)
+    return bytes(stream_output.getvalue())
--- backend/pdfcc/views.py
+++ backend/pdfcc/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .utility import analyse, replace
+from .utility_analyze import analyse_colors
+from .utility_process import process_request
 
 
 @csrf_exempt
@@ -12,7 +13,7 @@ def analyse(request):
     '''
     if len(request.POST) <= 1:
         # call the utility.new_analyse function to extract the color information from the PDF
-        color_list = analyse(request.FILES.get('file'))
+        color_list = analyse_colors(request.FILES.get('file'))
         if len(color_list) < 1:
             # if no colors are found in the PDF, return a response indicating the problem
             return JsonResponse({'error': True, 'message': 'No Colors found in PDF', 'analysis_result': color_list})
@@ -31,9 +32,9 @@ def process(request):
     '''
     try:
         # try to handle input and return new PDF encoded as Base64 if no Exeptions occur
-        b64, number_of_colors = replace(
+        b64 = process_request(
             request.POST, request.FILES.get('file'))
-        return JsonResponse({'error': False, 'message': 'Substituted Successfully', 'b64': b64, 'noc': number_of_colors})
+        return JsonResponse({'error': False, 'message': 'Substituted Successfully', 'b64': b64})
     except BaseException as e:
         # if an Exeption is raised during handling of the input, return the error message
         # most likely cause for this is if no (valid) colors are provided for substitution
